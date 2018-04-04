4dc899455ab2e4aada1bf6a0dbaac31c53e93b61
Darren Eggenschwiler <deggen@Darrens-MacBook-Pro.local>
2018-04-04 13:14:24 +0100

    not finding first file

--- search.php
+++ search.php
@@ -159,14 +159,14 @@ else{
     echo "<h3 style=\"padding-top: 85px;\" id=\"table" . $county . "\">Folders in " . $table . "</h3> <ul class='sleek'>";
     }
     else {$closethis = 0;}
-  //  while (mysqli_fetch_array($result,MYSQLI_ASSOC)){
+    while (mysqli_fetch_array($result,MYSQLI_ASSOC)){
         echo '<li>';
         foreach($row as $field) {
             echo $field;
         }
         echo '</li>';
         $resultcount += 1; //add these findings to the number of found items in this table.
-  //  }
+    }
     $drivecount[] = $resultcount;
     if ($closethis == 1){echo "</ul><hr><br />";}
     $county = $county + 1; //increment to control table id through the foreach loop so we can pabel each table differently.
