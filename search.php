50655a7825493c0efcfa5bd4fe3a4b523d68a352
Darren Eggenschwiler <deggen@Darrens-MacBook-Pro.local>
2018-04-04 13:34:10 +0100

    not finding first file

--- search.php
+++ search.php
@@ -127,14 +127,14 @@ if($depth=="file"){
       echo "<h3 style=\"padding-top: 85px;\" id=\"table" . $county . "\">Files in " . $table . "</h3><ul class='sleek'>";
       }
       else {$closethis = 0;}
-      while (false){
+      do {
           echo '<li>';
           foreach($row as $field) {
               echo $field;
           }
           echo '</li>';
           $resultcount += 1;//add these findings to the number of found items in this table.
-      }
+      } while($row = mysqli_fetch_array($result,MYSQLI_ASSOC))
       $drivecount[] = $resultcount;
       if ($closethis == 1){echo "</ul><hr><br />";}
       $county = $county + 1; //increment to control table id through the foreach loop so we can pabel each table differently.
@@ -159,7 +159,7 @@ else{
     echo "<h3 style=\"padding-top: 85px;\" id=\"table" . $county . "\">Folders in " . $table . "</h3> <ul class='sleek'>";
     }
     else {$closethis = 0;}
-    while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+    do {
         echo '<li>';
         echo $row;
         foreach($row as $field) {
@@ -167,7 +167,7 @@ else{
         }
         echo '</li>';
         $resultcount += 1; //add these findings to the number of found items in this table.
-    }
+    } while($row = mysqli_fetch_array($result,MYSQLI_ASSOC));
     $drivecount[] = $resultcount;
     if ($closethis == 1){echo "</ul><hr><br />";}
     $county = $county + 1; //increment to control table id through the foreach loop so we can pabel each table differently.
