a72215c683705f2939c842a28d9b170252f06ed9
Darren Eggenschwiler <deggen@Darrens-MacBook-Pro.local>
2018-04-04 13:10:54 +0100

    not finding first file

--- search.php
+++ search.php
@@ -159,7 +159,7 @@ else{
     echo "<h3 style=\"padding-top: 85px;\" id=\"table" . $county . "\">Folders in " . $table . "</h3> <ul class='sleek'>";
     }
     else {$closethis = 0;}
-    echo "<script>alert('" + str(mysqli_fetch_array($result,MYSQLI_ASSOC)) + "');</script>";
+    echo "<script>alert('" + String(mysqli_fetch_array($result,MYSQLI_ASSOC)) + "');</script>";
     while (mysqli_fetch_array($result,MYSQLI_ASSOC)){
         echo '<li>';
         foreach($row as $field) {
